7fd27f519a2963fff0c5f8215474aca9789903d6
bharath kumar MS <bharath2005goo@gmail.com>
2025-11-12 14:00:40 +0530

    final update 1

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -30,7 +30,7 @@ export const registerUser = createAsyncThunk(
   async (formData, { rejectWithValue }) => {
       try {
           const response = await axios.post(
-              "https://million-minds-research.onrender.com/auth/register",
+              "https://million-minds-research.onrender.com/api/auth/register",
               formData,
               { withCredentials: true }
           );
@@ -46,7 +46,7 @@ export const loginUser = createAsyncThunk(
   "/auth/login",
   async (formData) => {
     const response = await axios.post(
-      "https://million-minds-research.onrender.com/auth/login",
+      "https://million-minds-research.onrender.com/api/auth/login",
       formData,
       {
         withCredentials: true,
@@ -63,7 +63,7 @@ export const googleLogin = createAsyncThunk(
     // console.log("Slice Enter", token);
     
     const response = await axios.post(
-      "https://million-minds-research.onrender.com/auth/google",  // ✅ Corrected endpoint
+      "https://million-minds-research.onrender.com/api/auth/google",  // ✅ Corrected endpoint
       { token }, // Send token inside request body
       { withCredentials: true }
     );
@@ -82,7 +82,7 @@ export const logoutUser = createAsyncThunk(
 
   async () => {
     const response = await axios.post(
-      "https://million-minds-research.onrender.com/auth/logout",
+      "https://million-minds-research.onrender.com/api/auth/logout",
       {},
       {
         withCredentials: true,
@@ -104,7 +104,7 @@ export const checkAuth = createAsyncThunk(
 
   async () => {
     const response = await axios.get(
-      "https://million-minds-research.onrender.com/auth/check-auth",
+      "https://million-minds-research.onrender.com/api/auth/check-auth",
       {
         withCredentials: true,
         headers: {
